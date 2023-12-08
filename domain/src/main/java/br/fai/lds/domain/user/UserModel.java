6a7abf38b3dfc833530ee82c48a3667dedd8e55b
Nathan Faria <nathanfaria607@gmail.com>
2023-12-08 16:00:43 -0300

    feat: add builder from lomnok in usermodel

--- domain/src/main/java/br/fai/lds/domain/user/UserModel.java
+++ domain/src/main/java/br/fai/lds/domain/user/UserModel.java
@@ -1,12 +1,14 @@
 package br.fai.lds.domain.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserModel {
     private int id;
     private String username;
